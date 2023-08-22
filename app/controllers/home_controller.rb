8c816522c71c834fc15204f413d6b93d71241c8b
ymmtd0x0b <ymmtd0x0b@gmail.com>
2023-08-23 08:39:17 +0900

    ホームへのリクエストにおける分岐条件を変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,13 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    redirect_to repository_issues_path(current_user.repositories.first) + '?target=assigned' if session[:user_id]
+    if logged_in?
+      if Repository.find_by(user_id: session[:user_id]).presence
+        redirect_to repository_issues_path(current_user.repositories.first) + '?target=assigned' if session[:user_id]
+      else
+        flash[:error] = 'リポジトリが登録されていません'
+        redirect_to new_repository_path
+      end
+    end
   end
 end
