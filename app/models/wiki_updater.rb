0c1fc97497634f15d8ffad44e48cef08dd5e4919
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-02-26 01:18:12 +0900

    fixup

--- app/models/wiki_updater.rb
+++ app/models/wiki_updater.rb
@@ -3,12 +3,12 @@ class WikiUpdater
     repository = options[:repository]
     user = options[:user]
 
-    wikis = Git::Wiki.created_by(repository, user)
-    return nil if wikis.empty?
-
     # Wiki は API が提供されていない都合上、取得したデータと保存済みデータを
     # 照合することができないので、保存済みデータを一旦全削除して取得したデータ再登録する
-    user.wikis.destroy_all
+    user.wikis.where(repository_id: repository.id).destroy_all
+
+    wikis = Git::Wiki.created_by(repository, user)
+    return nil if wikis.empty?
 
     Insert.wiki(wikis)
   end
