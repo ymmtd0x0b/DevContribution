73c55c820247d06d447b2b9da5a98a5597cec38c
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-03-13 20:12:34 +0900

    SVG画像の表示機能を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,14 @@ module ApplicationHelper
 
     link_to tab_name, path, class: classes
   end
+
+  def icon(icon, options = {})
+    file = File.read("app/assets/images/#{icon}.svg")
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css 'svg'
+    if options[:class].present?
+      svg['class'] += " " + options[:class]
+    end
+      doc.to_html.html_safe
+  end
 end
