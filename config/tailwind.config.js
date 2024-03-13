9d6719e33638661cccdf0ec2805c38c9ee4888b0
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-03-13 13:51:33 +0900

    daisyUIとBulumaを削除

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -19,10 +19,6 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-    require('daisyui')
-  ],
-  daisyui: {
-    themes: ['emerald'],
-  },
+    require('@tailwindcss/container-queries')
+  ]
 }
