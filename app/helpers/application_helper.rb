886bf2dd0d082f5822b2a93dd6045dab2b157189
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-03-14 16:15:31 +0900

    ホーム画面のタブのFlowbite化

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
-  def tab_to(tab_name, path, classes = '')
-    classes = classes.split(' ') + %w[h-10 w-32 text-center leading-10 border-t border-x border-gray-400]
-    classes << 'tab-active' if current_page?(path)
-
-    link_to tab_name, path, class: classes
+  def tab_to(tab_name, path)
+    if current_page?(path)
+      link_to tab_name, path, class: "inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500", aria: { current: "page" }
+    else
+      link_to tab_name, path, class: "inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+    end
   end
 
   def icon(icon, options = {})
