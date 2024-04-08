2291e7b1edf9290cdc80bc95701d2eaac52f5ec6
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-04-09 03:05:32 +0900

    リポジトリのルーティング表現をを単数形に変更

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'auth/github/callback', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
   resource :retirement, only: %i[create], controller: 'retirement'
-  resources :repositories, only: %i[update]
+  resource :repository, only: %i[update]
   resources :users, only: [], path: :contributors do
     resources :issues,  only: %i[index]
     resources :wikis, only: %i[index]
