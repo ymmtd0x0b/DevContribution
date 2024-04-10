15bb5b8020563681c7ddb960138747223ec9d5b9
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-04-10 19:51:34 +0900

    ログインの度にIssue/Wikiを取得してしまうバグを修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,7 +4,7 @@ class UserSessionsController < ApplicationController
   def create
     user = User.find_or_create_by_github_auth!(request.env['omniauth.auth'])
     session[:user_id] = user.id
-    if user.new_record?
+    if !user.new_record?
       flash[:info] = 'ログインしました'
     else
       session[:newspaper] = 'user_create'
