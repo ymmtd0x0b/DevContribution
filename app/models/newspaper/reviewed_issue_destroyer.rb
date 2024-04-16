a923d55e1201e9af55c53be73b63da9ce81bd3dc
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-04-17 08:33:33 +0900

    fixup

--- app/models/newspaper/reviewed_issue_destroyer.rb
+++ app/models/newspaper/reviewed_issue_destroyer.rb
@@ -1,13 +1,13 @@
 module Newspaper
   class ReviewedIssueDestroyer
     def call(user)
-      destroy_reviewed_issues(user)
-      destory_reviewed_pull_requests(user)
+      destroy_reviewed_issues_not_referenced_by_other_users(user)
+      destory_reviewed_pull_requests_not_referenced_by_other_users(user)
     end
 
     private
 
-    def destroy_reviewed_issues(user)
+    def destroy_reviewed_issues_not_referenced_by_other_users(user)
       issues_id = user.reviewed_issues.ids
       issues_id_referenced_by_other_users = Issue.joins(:reviews, :assigns)
         .where('reviews.reviewable_id in (?) and reviews.user_id != ? or assigns.assignable_id in (?)', issues_id, user.id, issues_id)
@@ -16,7 +16,7 @@ module Newspaper
       user.reviewed_issues.where.not(id: issues_id_referenced_by_other_users).destroy_all
     end
 
-    def destory_reviewed_pull_requests(user)
+    def destory_reviewed_pull_requests_not_referenced_by_other_users(user)
       pull_requests_id = user.reviewed_pull_requests.ids
       pull_requests_id_referenced_by_other_users = PullRequest.joins(:reviews, :assigns)
         .where('reviews.reviewable_id in (?) and reviews.user_id != ? or assigns.assignable_id in (?)', pull_requests_id, user.id, pull_requests_id)
