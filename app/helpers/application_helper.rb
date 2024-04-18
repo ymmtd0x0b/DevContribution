536fabcc1cf10d175df36aee82c65292b0624643
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-04-18 09:57:47 +0900

    デザイン調整

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def tab_to(tab_name, path)
     if current_page?(path, check_parameters: true)
-      link_to tab_name, path, class: "inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500", aria: { current: "page" }
+      link_to tab_name, path, class: "inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active", aria: { current: "page" }
     else
-      link_to tab_name, path, class: "inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+      link_to tab_name, path, class: "inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300"
     end
   end
 
