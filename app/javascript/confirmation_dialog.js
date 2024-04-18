63ff8cb03b75dd6132216f69ccd3231daa24a26a
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-04-18 10:20:26 +0900

    SweetAlert2のデザインを調整

--- app/javascript/confirmation_dialog.js
+++ app/javascript/confirmation_dialog.js
@@ -4,9 +4,12 @@ import loadingModal from './loading_modal.js'
 Turbo.setConfirmMethod((message, element) => {
   return new Promise((resolve, reject) => {
     Swal.fire({
-      text: message || '本当に実行しますか？',
+      title: message || '本当に実行しますか？',
       icon: 'warning',
-      showCancelButton: true
+      showCancelButton: true,
+      customClass: {
+        title: 'text-xl text-slate-600 font-medium'
+      },
     }).then((result) => {
       const id = element.getAttribute('id')
       if(result.isConfirmed && id == 'repository_update_button') {
