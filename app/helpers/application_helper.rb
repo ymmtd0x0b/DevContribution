17e4807171b0ef7d87b4e32a3d6373f1df09be1d
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-04-22 18:39:26 +0900

    svgの表示に使用するビューヘルパーメソッド内の  html_safe を避けるため image_tag へ変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,15 +8,4 @@ module ApplicationHelper
       link_to tab_name, path, class: 'inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300'
     end
   end
-
-  def icon(icon, options = {})
-    file = File.read("app/assets/images/#{icon}.svg")
-    doc = Nokogiri::HTML::DocumentFragment.parse file
-    svg = doc.at_css 'svg'
-    if options[:class].present?
-      svg['class'] = [svg['class'], options[:class]].join(' ')
-    else
-      doc.to_html.html_safe
-    end
-  end
 end
