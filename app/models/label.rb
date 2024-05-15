f433ae8095575af12899d54aa5a4e6dcf656691d
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-05-15 16:33:40 +0900

    不要なコードを削除

--- app/models/label.rb
+++ app/models/label.rb
@@ -2,14 +2,4 @@
 
 class Label < ApplicationRecord
   belongs_to :repository
-
-  class << self
-    def synchronize_with_github_by(repository)
-      labels = Github::Label.find_by(repository)
-      return nil if labels.nil?
-
-      repository.labels.where.not(id: labels.map(&:id)).delete_all
-      upsert_all(labels.map(&:to_h), unique_by: %i[repository_id name]) if labels.any?
-    end
-  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,19 +3,4 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :pull_request
-
-  class << self
-    def synchronize(model_name, items, user)
-      if items.empty?
-        user.reviews.where(reviewable_type: model_name).delete_all
-      else
-        user.reviews
-            .where('reviewable_type = ? and reviewable_id not in (?)', model_name, items.map(&:id))
-            .delete_all
-
-        reviews_data = items.map { |item| { reviewable_type: model_name, reviewable_id: item.id, user_id: user.id } }
-        insert_all reviews_data, unique_by: %i[reviewable_id user_id]
-      end
-    end
-  end
 end
