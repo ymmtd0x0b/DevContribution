8c1156626dc4e25e39101cff7c286e42aeded43a
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-05-22 02:28:29 +0900

    IssueDecoratorのスペックを作成

---
+++ spec/decorators/issue_decorator_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe IssueDecorator do
+  describe '#url' do
+    it '「リポジトリのURL / issues / Issueのナンバー」に変換したURLを返すこと' do
+      issue = FactoryBot.create(:issue, number: 123, repository: FactoryBot.create(:repository, url: 'https://example.com/test_repository'))
+      decorator_issue = ActiveDecorator::Decorator.instance.decorate(issue)
+
+      expect(decorator_issue.url).to eq 'https://example.com/test_repository/issues/123'
+    end
+  end
+
+  describe '#point' do
+    context 'ラベルが貼られていない場合' do
+      it 'ゼロを返すこと' do
+        decorator_issue = ActiveDecorator::Decorator.instance.decorate(FactoryBot.create(:issue))
+        expect(decorator_issue.point).to eq 0
+      end
+    end
+
+    context 'ラベルが貼られている場合' do
+      it 'ストーリーポイントとなるラベルがあれば、そのポイントを返すこと' do
+        issue = FactoryBot.create(:issue) do |created_issue|
+          created_issue.labelings.create!(label: FactoryBot.create(:label, name: '1'))
+          created_issue.labelings.create!(label: FactoryBot.create(:label, name: 'good first issue'))
+        end
+        decorator_issue = ActiveDecorator::Decorator.instance.decorate(issue)
+
+        expect(decorator_issue.point).to eq 1
+      end
+
+      it 'ストーリーポイントとなるラベルがなけれが、ゼロを返すこと' do
+        issue = FactoryBot.create(:issue) do |created_issue|
+          created_issue.labelings.create!(label: FactoryBot.create(:label, name: 'good first issue'))
+        end
+        decorator_issue = ActiveDecorator::Decorator.instance.decorate(issue)
+
+        expect(decorator_issue.point).to eq 0
+      end
+    end
+  end
+end
