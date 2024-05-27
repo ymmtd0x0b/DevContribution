4b543c1191aa24847ed85997023e35c3930f8175
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-05-27 12:29:34 +0900

    GitHub::PullRequest モデルの更新＆スペックを作成

--- app/models/github/pull_request.rb
+++ app/models/github/pull_request.rb
@@ -4,11 +4,11 @@ module Github
   class PullRequest
     attr_reader :id, :issues_number
 
-    def initialize(repository_id, pull_request)
-      @id = pull_request.id
+    def initialize(repository_id:, pull_request: { id:, number:, issues_number: })
+      @id = pull_request[:id]
       @repository_id = repository_id
-      @number = pull_request.number
-      @issues_number = scan_issues_number(pull_request.body)
+      @number = pull_request[:number]
+      @issues_number = pull_request[:issues_number]
     end
 
     def to_h
@@ -23,26 +23,34 @@ module Github
 
     class << self
       def assigned_by(repository, user)
-        pull_requests = Github::Repository.search_issues("repo:#{repository.name} is:pr assignee:#{user.login} -label:release")
-        pull_requests.map { |pull_request| Github::PullRequest.new(repository.id, pull_request) }
+        client = Github::ApiClient.new
+        pull_requests = client.search_issues("repo:#{repository.name} is:pr assignee:#{user.login} -label:release")
+        pull_requests.map { |pull_request| Github::PullRequest.new(repository_id: repository.id, pull_request: convert_to_hash(pull_request)) }
       end
 
       def reviewed_by(repository, user)
-        # pull_requests = Github::Repository.search_issues("repo:#{repository.name} is:pr reviewed-by:#{user.login} review:approved -assignee:#{user.login}")
-        pull_requests = Github::Repository.search_issues("repo:#{repository.name} is:pr user-review-requested:#{user.login} -assignee:#{user.login}")
-        # pull_requests = Github::Repository.search_issues("repo:#{repository.name} is:pr reviewed-by:#{user.login} review:approved -assignee:#{user.login}")
-        pull_requests.map { |pull_request| Github::PullRequest.new(repository.id, pull_request) }
+        client = Github::ApiClient.new
+        # pull_requests = client.search_issues("repo:#{repository.name} is:pr reviewed-by:#{user.login} review:approved -assignee:#{user.login}")
+        pull_requests = client.search_issues("repo:#{repository.name} is:pr user-review-requested:#{user.login} -assignee:#{user.login}")
+        # pull_requests = client.search_issues("repo:#{repository.name} is:pr reviewed-by:#{user.login} review:approved -assignee:#{user.login}")
+        pull_requests.map { |pull_request| Github::PullRequest.new(repository_id: repository.id, pull_request: convert_to_hash(pull_request)) }
       end
-    end
 
-    private
+      private
 
-    def scan_issues_number(body)
-      issue_section = body.slice(/# [Ii]ssue.+# 概要/m)
-      return [] if issue_section.blank?
+      def convert_to_hash(pull_request)
+        { id: pull_request.id,
+          number: pull_request.number,
+          issues_number: scan_issues_number(pull_request.body) }
+      end
 
-      issue_urls = issue_section.scan(%r{http.+/issues/\d+|#\d+})
-      issue_urls.map { |issue_url| issue_url.slice(/\d+$/).to_i }.uniq
+      def scan_issues_number(body)
+        issue_section = body.slice(/# [Ii]ssue.+# 概要/m)
+        return [] if issue_section.blank?
+
+        issue_urls = issue_section.scan(%r{http.+/issues/\d+|#\d+})
+        issue_urls.map { |issue_url| issue_url.slice(/\d+$/).to_i }.uniq
+      end
     end
   end
 end
---
+++ spec/models/github/pull_request_spec.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Github::PullRequest, type: :model do
+  describe '#to_h' do
+    it '自身をハッシュ(連想配列)へ変換して返すこと( issues_number は含まない )' do
+      pull_request_data = { id: 111, number: 222, issues_number: [333, 444] }
+      pull_request = Github::PullRequest.new(repository_id: 123, pull_request: pull_request_data)
+
+      expect(pull_request.to_h).to eq({ repository_id: 123, id: 111, number: 222 })
+    end
+  end
+
+  describe '#create_pseudo_resolutions' do
+    it 'PullRequest ID と Issue Number をハッシュとして要素に持つ Array を返す' do
+      pull_request_data = { id: 111, number: 222, issues_number: [333, 444] }
+      pull_request = Github::PullRequest.new(repository_id: 123, pull_request: pull_request_data)
+
+      expect(pull_request.create_pseudo_resolutions).to eq([{ pull_request_id: 111, issue_number: 333 },
+                                                            { pull_request_id: 111, issue_number: 444 }])
+    end
+  end
+
+  describe '.assigned_by' do
+    context '該当する PullRequest がある場合' do
+      it 'Github::PullRequest オブジェクトを要素に持つ Array を返すこと', vcr: { cassette_name: 'github/pull_request/assigned_by' } do
+        repository = FactoryBot.create(:repository, name: 'ymmtd0x0b/for_test2')
+        user = FactoryBot.create(:user, login: 'ymmtd0x0b')
+
+        pull_requests = Github::PullRequest.assigned_by(repository, user)
+        expect(pull_requests).not_to be_empty
+        expect(pull_requests).to all(be_instance_of(Github::PullRequest))
+      end
+    end
+
+    context '該当する PullRequest がない場合' do
+      it '空の Array を返すこと', vcr: { cassette_name: 'github/pull_request/assigned_by_not_found' } do
+        repository = FactoryBot.create(:repository, name: 'ymmtd0x0b/for_test2')
+        user = FactoryBot.create(:user, login: 'not_exist_user')
+
+        pull_requests = Github::PullRequest.assigned_by(repository, user)
+        expect(pull_requests).to be_empty
+      end
+    end
+  end
+
+  describe '.reviewed_by' do
+    context '該当する PullRequest がある場合' do
+      it 'Github::PullRequest オブジェクトを要素に持つ Array を返すこと', vcr: { cassette_name: 'github/pull_request/reviewed_by' } do
+        repository = FactoryBot.create(:repository, name: 'ymmtd0x0b/for_test2')
+        user = FactoryBot.create(:user, login: 'ymmtd0x0b')
+
+        pull_requests = Github::PullRequest.reviewed_by(repository, user)
+        expect(pull_requests).not_to be_empty
+        expect(pull_requests).to all(be_instance_of(Github::PullRequest))
+      end
+    end
+
+    context '該当する PullRequest がない場合' do
+      it '空の Array を返すこと', vcr: { cassette_name: 'github/pull_request/reviewed_by_not_found' } do
+        repository = FactoryBot.create(:repository, name: 'ymmtd0x0b/for_test2')
+        user = FactoryBot.create(:user, login: 'not_exist_user')
+
+        pull_requests = Github::PullRequest.reviewed_by(repository, user)
+        expect(pull_requests).to be_empty
+      end
+    end
+  end
+end
