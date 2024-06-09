0a224abbc7c83e6877aceeae985012dd4d32a955
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-06-10 02:38:01 +0900

    Newspaper::Synchronizer クラスのスペックを作成

---
+++ spec/models/newspaper/synchronizer_spec.rb
@@ -0,0 +1,293 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Newspaper::Synchronizer, type: :model do
+  describe '.synchronize_issues' do
+    before do
+      FactoryBot.create(:repository, id: 1)
+    end
+
+    context '引数に渡されたデータの中に「未登録の Issue 」がある場合' do
+      it '新たに登録すること' do
+        FactoryBot.create(:issue, id: 100, title: 'issue#100', number: 100)
+
+        expect do
+          issues_collected_by_the_github_api = [
+            Github::Issue.new(repository_id: 1, issue: { id: 100, user_id: 1, title: 'issue#100', number: 100, labels_id: [] }),
+            Github::Issue.new(repository_id: 1, issue: { id: 200, user_id: 1, title: 'issue#200', number: 200, labels_id: [] })
+          ]
+          Newspaper::Synchronizer.synchronize_issues(issues_collected_by_the_github_api)
+        end.to change { Issue.count }.from(1).to(2)
+      end
+    end
+
+    context '引数に渡されたデータの中に「登録済みの Issue 」がある場合' do
+      it '該当 Issue の情報を更新すること' do
+        issue = FactoryBot.create(:issue, id: 100, title: 'before update...')
+
+        expect do
+          issues_collected_by_the_github_api = [
+            Github::Issue.new(repository_id: 1, issue: { id: 100, user_id: 1, title: 'updated!', number: 100, labels_id: [] })
+          ]
+          Newspaper::Synchronizer.synchronize_issues(issues_collected_by_the_github_api)
+        end.to change { issue.reload.title }.from('before update...').to('updated!')
+      end
+    end
+
+    it 'Issue のラベリングを同期させる(メソッドを呼び出す)こと' do
+      expect(Newspaper::Synchronizer).to receive(:synchronize_labelings)
+
+      issues_collected_by_the_github_api = [
+        Github::Issue.new(repository_id: 1, issue: { id: 100, user_id: 1, title: 'issue#100', number: 100, labels_id: [123] })
+      ]
+      Newspaper::Synchronizer.synchronize_issues(issues_collected_by_the_github_api)
+    end
+  end
+
+  describe '.synchronize_labelings' do
+    before do
+      FactoryBot.create(:repository, id: 1) do |repository|
+        FactoryBot.create(:label, repository:, id: 123)
+        FactoryBot.create(:label, repository:, id: 456)
+      end
+    end
+
+    context '引数に渡された Issue の labels_id に、既存のアソシエーションに該当しないラベル情報が存在する場合' do
+      it '対象ラベルと Issue のアソシエーションを登録する' do
+        issue = FactoryBot.create(:issue, id: 100) { |created_issue| created_issue.labelings.create!(label_id: 123) }
+
+        expect do
+          issues_collected_by_the_github_api = [Github::Issue.new(repository_id: 1, issue: { id: 100, labels_id: [123, 456] })]
+          Newspaper::Synchronizer.send(:synchronize_labelings, issues_collected_by_the_github_api)
+        end.to change { issue.labels.pluck(:id) }.from([123]).to([123, 456])
+      end
+    end
+
+    context '引数に渡された Issue の labels_id に、既存のアソシエーションのラベル情報が存在しない場合' do
+      it '対象のアソシエーションを削除する' do
+        issue = FactoryBot.create(:issue, id: 100) do |created_issue|
+          created_issue.labelings.create!(label_id: 123)
+          created_issue.labelings.create!(label_id: 456)
+        end
+
+        expect do
+          issues_collected_by_the_github_api = [Github::Issue.new(repository_id: 1, issue: { id: 100, labels_id: [123] })]
+          Newspaper::Synchronizer.send(:synchronize_labelings, issues_collected_by_the_github_api)
+        end.to change { issue.labels.pluck(:id) }.from([123, 456]).to([123])
+      end
+    end
+  end
+
+  describe '.synchronize_pull_requests' do
+    before do
+      FactoryBot.create(:repository, id: 1)
+    end
+
+    context '引数に渡されたデータの中に「未登録のPullRequest」がある場合' do
+      it '新たに登録すること' do
+        FactoryBot.create(:pull_request, id: 100, number: 100)
+
+        expect do
+          pull_requests_collected_by_the_github_api = [
+            Github::PullRequest.new(repository_id: 1, pull_request: { id: 100, number: 100, issues_number: [] }),
+            Github::PullRequest.new(repository_id: 1, pull_request: { id: 200, number: 200, issues_number: [] })
+          ]
+          Newspaper::Synchronizer.synchronize_pull_requests(pull_requests_collected_by_the_github_api)
+        end.to change { PullRequest.count }.from(1).to(2)
+      end
+    end
+
+    context '引数に渡されたデータの中に「登録済みのPullRequest」がある場合' do
+      it '対象の情報を更新すること' do
+        pull_request = FactoryBot.create(:pull_request, id: 100, number: 100)
+
+        expect do
+          pull_requests_collected_by_the_github_api = [
+            Github::PullRequest.new(repository_id: 1, pull_request: { id: 100, number: 999, issues_number: [] })
+          ]
+          Newspaper::Synchronizer.synchronize_pull_requests(pull_requests_collected_by_the_github_api)
+        end.to change { pull_request.reload.number }.from(100).to(999)
+      end
+    end
+  end
+
+  describe '.synchronize_resolutions' do
+    context '引数に渡された PullRequest の issues_number 属性にデータがある場合' do
+      context '一致する Issue がデータベースに存在すれば' do
+        it 'アソシエーションを新たに登録すること' do
+          FactoryBot.create(:issue, id: 100, number: 100)
+          FactoryBot.create(:issue, id: 200, number: 200)
+          pull_request = FactoryBot.create(:pull_request, id: 300, number: 300)
+
+          expect do
+            pull_requests_collected_by_the_github_api = [
+              Github::PullRequest.new(repository_id: 1, pull_request: { id: 300, number: 300, issues_number: [100, 200] })
+            ]
+            Newspaper::Synchronizer.synchronize_resolutions(pull_requests_collected_by_the_github_api)
+          end.to change { pull_request.issues.pluck(:id) }.from([]).to([100, 200])
+        end
+      end
+
+      context '一致する Issue がデータベースに存在しなければ' do
+        it 'アソシエーションを登録しない(できない)こと' do
+          FactoryBot.create(:issue, id: 100, number: 100)
+          pull_request = FactoryBot.create(:pull_request, id: 200, number: 200)
+
+          expect do
+            pull_requests_collected_by_the_github_api = [
+              Github::PullRequest.new(repository_id: 1, pull_request: { id: 200, number: 200, issues_number: [100, 999] })
+            ]
+            Newspaper::Synchronizer.synchronize_resolutions(pull_requests_collected_by_the_github_api)
+          end.to change { pull_request.issues.pluck(:id) }.from([]).to([100])
+        end
+      end
+
+      context '登録済みアソシエーション先の Issue が含まれて無ければ' do
+        it '対象アソシエーションを削除すること' do
+          pull_request = FactoryBot.create(:pull_request, id: 300, number: 300) do |created_pull_request|
+            created_pull_request.resolutions.create!(issue: FactoryBot.create(:issue, id: 100, number: 100))
+            created_pull_request.resolutions.create!(issue: FactoryBot.create(:issue, id: 200, number: 200))
+          end
+
+          expect do
+            pull_requests_collected_by_the_github_api = [
+              Github::PullRequest.new(repository_id: 123, pull_request: { id: 300, number: 300, issues_number: [100] })
+            ]
+            Newspaper::Synchronizer.synchronize_resolutions(pull_requests_collected_by_the_github_api)
+          end.to change { pull_request.issues.pluck(:id) }.from([100, 200]).to([100])
+        end
+      end
+    end
+
+    context '引数に渡された PullRequest の issues_number 属性にデータがない場合' do
+      it 'その PullRequest が持つ Issue とのアソシエーションを全て削除すること' do
+        pull_request = FactoryBot.create(:pull_request, id: 100, number: 100) do |created_pull_request|
+          created_pull_request.resolutions.create!(issue: FactoryBot.create(:issue))
+          created_pull_request.resolutions.create!(issue: FactoryBot.create(:issue))
+        end
+
+        expect do
+          pull_requests_collected_by_the_github_api = [
+            Github::PullRequest.new(repository_id: 1, pull_request: { id: 100, number: 100, issues_number: [] })
+          ]
+          Newspaper::Synchronizer.synchronize_resolutions(pull_requests_collected_by_the_github_api)
+        end.to change { pull_request.issues.count }.from(2).to(0)
+      end
+    end
+  end
+
+  describe '.synchronize_assigns' do
+    context '引数に渡されたデータの中に、既存のアソシエーションに該当しない Issue が存在する場合' do
+      it '対象の Issue とユーザーのアソシエーションを登録する' do
+        FactoryBot.create(:issue, id: 100)
+        FactoryBot.create(:issue, id: 200)
+
+        user = FactoryBot.create(:user) { |created_user| created_user.assigns.create!(assignable_type: 'Issue', assignable_id: 100) }
+
+        expect do
+          issues_collected_by_the_github_api = [
+            Github::Issue.new(repository_id: 1, issue: { id: 100, user_id: 1, title: 'issue#100', number: 100, labels_id: [] }),
+            Github::Issue.new(repository_id: 1, issue: { id: 200, user_id: 1, title: 'issue#200', number: 200, labels_id: [] })
+          ]
+          Newspaper::Synchronizer.synchronize_assigns('Issue', issues_collected_by_the_github_api, user)
+        end.to change { user.assigned_issues.pluck(:id) }.from([100]).to([100, 200])
+      end
+    end
+
+    context '引数に渡されたデータの中に、既存のアソシエーションに該当する Issue が存在しない場合' do
+      it '対象の Issue とユーザーのアソシエーションを削除する' do
+        FactoryBot.create(:issue, id: 100)
+        FactoryBot.create(:issue, id: 200)
+
+        user = FactoryBot.create(:user) do |created_user|
+          created_user.assigns.create!(assignable_type: 'Issue', assignable_id: 100)
+          created_user.assigns.create!(assignable_type: 'Issue', assignable_id: 200)
+        end
+
+        expect do
+          issues_collected_by_the_github_api = [
+            Github::Issue.new(repository_id: 1, issue: { id: 100, user_id: 1, title: 'issue#100', number: 100, labels_id: [] })
+          ]
+          Newspaper::Synchronizer.synchronize_assigns('Issue', issues_collected_by_the_github_api, user)
+        end.to change { user.assigned_issues.pluck(:id) }.from([100, 200]).to([100])
+      end
+    end
+
+    context '引数に渡されたデータの中に、既存のアソシエーションに該当しない PullRequest が存在する場合' do
+      it '対象の PullRequest とユーザーのアソシエーションを登録する' do
+        FactoryBot.create(:pull_request, id: 100)
+        FactoryBot.create(:pull_request, id: 200)
+
+        user = FactoryBot.create(:user) do |created_user|
+          created_user.assigns.create!(assignable_type: 'PullRequest', assignable_id: 100)
+        end
+
+        expect do
+          pull_requests_collected_by_the_github_api = [
+            Github::PullRequest.new(repository_id: 1, pull_request: { id: 100, number: 100, issues_number: [] }),
+            Github::PullRequest.new(repository_id: 1, pull_request: { id: 200, number: 200, issues_number: [] })
+          ]
+          Newspaper::Synchronizer.synchronize_assigns('PullRequest', pull_requests_collected_by_the_github_api, user)
+        end.to change { user.assigned_pull_requests.pluck(:id) }.from([100]).to([100, 200])
+      end
+    end
+
+    context '引数に渡されたデータの中に、既存のアソシエーションに該当する PullRequest が存在しない場合' do
+      it '対象の PullRequest とユーザーのアソシエーションを削除する' do
+        FactoryBot.create(:pull_request, id: 100)
+        FactoryBot.create(:pull_request, id: 200)
+
+        user = FactoryBot.create(:user) do |created_user|
+          created_user.assigns.create!(assignable_type: 'PullRequest', assignable_id: 100)
+          created_user.assigns.create!(assignable_type: 'PullRequest', assignable_id: 200)
+        end
+
+        expect do
+          pull_requests_collected_by_the_github_api = [
+            Github::PullRequest.new(repository_id: 1, pull_request: { id: 100, number: 100, issues_number: [] })
+          ]
+          Newspaper::Synchronizer.synchronize_assigns('PullRequest', pull_requests_collected_by_the_github_api, user)
+        end.to change { user.assigned_pull_requests.pluck(:id) }.from([100, 200]).to([100])
+      end
+    end
+  end
+
+  describe '.synchronize_reviews' do
+    context '引数に渡されたデータの中に、既存のアソシエーションに該当しない PullRequest が存在する場合' do
+      it '対象の PullRequest とユーザーのアソシエーションを登録する' do
+        FactoryBot.create(:pull_request, id: 100)
+        FactoryBot.create(:pull_request, id: 200)
+
+        user = FactoryBot.create(:user) { |created_user| created_user.reviews.create!(pull_request_id: 100) }
+
+        expect do
+          pull_requests_collected_by_the_github_api = [
+            Github::PullRequest.new(repository_id: 1, pull_request: { id: 100, number: 100, issues_number: [] }),
+            Github::PullRequest.new(repository_id: 1, pull_request: { id: 200, number: 200, issues_number: [] })
+          ]
+          Newspaper::Synchronizer.synchronize_reviews(pull_requests_collected_by_the_github_api, user)
+        end.to change { user.reviewed_pull_requests.pluck(:id) }.from([100]).to([100, 200])
+      end
+    end
+
+    context '引数に渡されたデータの中に、既存のアソシエーションに該当する PullRequest が存在しない場合' do
+      it '対象の PullRequest とユーザーのアソシエーションを削除する' do
+        FactoryBot.create(:pull_request, id: 100)
+        FactoryBot.create(:pull_request, id: 200)
+
+        user = FactoryBot.create(:user) do |created_user|
+          created_user.reviews.create!(pull_request_id: 100)
+          created_user.reviews.create!(pull_request_id: 200)
+        end
+
+        expect do
+          pull_requests_collected_by_the_github_api = [
+            Github::PullRequest.new(repository_id: 1, pull_request: { id: 100, number: 100, issues_number: [] })
+          ]
+          Newspaper::Synchronizer.synchronize_reviews(pull_requests_collected_by_the_github_api, user)
+        end.to change { user.reviewed_pull_requests.pluck(:id) }.from([100, 200]).to([100])
+      end
+    end
+  end
+end
