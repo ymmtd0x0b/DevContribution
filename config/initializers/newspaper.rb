5b5dc15c78dff30f9c5b595b662d21ac8e138523
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-06-10 16:53:14 +0900

    Newspaperの修正

--- config/initializers/newspaper.rb
+++ config/initializers/newspaper.rb
@@ -2,12 +2,15 @@ Rails.configuration.to_prepare do
   Newspaper.subscribe(:user_create, Newspaper::AssignedIssueSynchronizer.new)
   Newspaper.subscribe(:user_create, Newspaper::AssignedPullRequestSynchronizer.new)
   Newspaper.subscribe(:user_create, Newspaper::ReviewedIssueSynchronizer.new)
+  Newspaper.subscribe(:user_create, Newspaper::ReviewedPullRequestSynchronizer.new)
   Newspaper.subscribe(:user_create, Newspaper::CreatedIssueSynchronizer.new)
   Newspaper.subscribe(:user_create, Newspaper::WikiSynchronizer.new)
 
   Newspaper.subscribe(:repository_update, Newspaper::LabelSynchronizer.new)
   Newspaper.subscribe(:repository_update, Newspaper::AssignedIssueSynchronizer.new)
+  Newspaper.subscribe(:repository_update, Newspaper::AssignedPullRequestSynchronizer.new)
   Newspaper.subscribe(:repository_update, Newspaper::ReviewedIssueSynchronizer.new)
+  Newspaper.subscribe(:repository_update, Newspaper::ReviewedPullRequestSynchronizer.new)
   Newspaper.subscribe(:repository_update, Newspaper::CreatedIssueSynchronizer.new)
   Newspaper.subscribe(:repository_update, Newspaper::WikiSynchronizer.new)
 
