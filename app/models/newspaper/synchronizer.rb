1f1e50648ea81238d8d3ce5429b194fdef851a9f
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-06-11 17:43:56 +0900

    同期処理の責務移動に伴う不要なクラス＆スペックを削除

--- app/models/newspaper/synchronizer.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module Newspaper::Synchronizer
-  class << self
-  end
-end
--- spec/models/newspaper/synchronizer_spec.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe Newspaper::Synchronizer, type: :model do
-end
