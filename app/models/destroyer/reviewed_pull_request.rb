82720cc7de269dd888cd1151e7c8aa5af189c881
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-06-27 07:18:17 +0900

    Destroyer::ReviewedPullRequest クラスのスペックを作成

--- app/models/destroyer/reviewed_pull_request.rb
+++ app/models/destroyer/reviewed_pull_request.rb
@@ -1,21 +1,24 @@
 # frozen_string_literal: true
 
 module Destroyer
-  class ReviewedIssue
+  class ReviewedPullRequest
     def call(user)
-      destory_reviewed_pull_requests_not_referenced_by_other_users(user)
+      pull_requests_id = user.reviewed_pull_requests.ids
+      pull_requests_id_referenced_by_other_users =
+        filter_pull_requests_assigned_by_other_users_from(pull_requests_id, user.id) +
+        filter_pull_requests_reviewed_by_other_users_from(pull_requests_id, user.id)
+
+      user.reviewed_pull_requests.where.not(id: pull_requests_id_referenced_by_other_users).destroy_all
     end
 
     private
 
-    def destory_reviewed_pull_requests_not_referenced_by_other_users(user)
-      pull_requests_id = user.reviewed_pull_requests.ids
-      pull_requests_id_referenced_by_other_users =
-        PullRequest.joins(:reviews, :assigns)
-                   .where('reviews.reviewable_id in (?) and reviews.user_id != ? or assigns.assignable_id in (?)', pull_requests_id, user.id, pull_requests_id)
-                   .ids
+    def filter_pull_requests_assigned_by_other_users_from(pull_requests_id, user_id)
+      PullRequest.joins(:assigns).where('pull_requests.id in (?) and assigns.user_id != ?', pull_requests_id, user_id).ids
+    end
 
-      user.reviewed_pull_requests.where.not(id: pull_requests_id_referenced_by_other_users).destroy_all
+    def filter_pull_requests_reviewed_by_other_users_from(pull_requests_id, user_id)
+      PullRequest.joins(:reviews).where('pull_requests.id in (?) and reviews.user_id != ?', pull_requests_id, user_id).ids
     end
   end
 end
