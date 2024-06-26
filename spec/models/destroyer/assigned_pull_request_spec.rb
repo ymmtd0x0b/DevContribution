7dcecbf76ecb5b5d84c6e7cf45dfd16ffb82bd92
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-06-27 04:52:04 +0900

    Destroyer::AssignedPullRequest クラスのスペックの可読性UP

--- spec/models/destroyer/assigned_pull_request_spec.rb
+++ spec/models/destroyer/assigned_pull_request_spec.rb
@@ -8,28 +8,29 @@ RSpec.describe Destroyer::AssignedPullRequest, type: :model do
     let(:alice) { FactoryBot.create(:user, login: 'alice') }
     let(:bob) { FactoryBot.create(:user, login: 'bob') }
 
-    it 'ユーザーがアサインしている PullRequest の内、他のユーザーが参照していないのは削除すること' do
-      FactoryBot.create(:pull_request, id: 100) { |pull_request| pull_request.assigns.create!(user: alice) }
-      FactoryBot.create(:pull_request, id: 200) do |pull_request|
+    it 'ユーザーがアサインしている PullRequest の内、「他のユーザーが参照していないもの」は「削除する」こと' do
+      FactoryBot.create(:pull_request) { |pull_request| pull_request.assigns.create!(user: alice) }
+      FactoryBot.create(:pull_request) do |pull_request|
         pull_request.assigns.create!(user: alice)
         pull_request.reviews.create!(user: alice)
       end
 
-      expect { assigned_pull_request_destroyer.call(alice) }.to change { PullRequest.pluck(:id) }.from([100, 200]).to([])
+      expect { assigned_pull_request_destroyer.call(alice) }.to change { alice.assigned_pull_requests.count }.from(2).to(0)
+                                                            .and change { PullRequest.count }.by(-2)
     end
 
-    it 'ユーザーがアサインしている PullRequest の内、他のユーザーが参照しているのは削除しないこと' do
-      FactoryBot.create(:pull_request, id: 100) do |pull_request|
+    it 'ユーザーがアサインしている PullRequest の内、「他のユーザーが参照しているもの」は「削除しない」こと' do
+      FactoryBot.create(:pull_request) do |pull_request|
         pull_request.assigns.create!(user: alice)
         pull_request.assigns.create!(user: bob)
       end
 
-      FactoryBot.create(:pull_request, id: 200) do |pull_request|
+      FactoryBot.create(:pull_request) do |pull_request|
         pull_request.assigns.create!(user: alice)
         pull_request.reviews.create!(user: bob)
       end
 
-      expect { assigned_pull_request_destroyer.call(alice) }.not_to change { PullRequest.pluck(:id) }.from([100, 200])
+      expect { assigned_pull_request_destroyer.call(alice) }.not_to change { alice.assigned_pull_requests.count }.from(2)
     end
   end
 end
