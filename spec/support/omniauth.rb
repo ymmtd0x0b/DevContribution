846ceda4c1611b074ac1bf5961d97b04606aa43f
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-07-04 15:20:22 +0900

    システムスペック用の OmniAuth ログインを作成

--- spec/support/omniauth.rb
+++ spec/support/omniauth.rb
@@ -2,3 +2,14 @@
 
 require 'omniauth'
 OmniAuth.config.test_mode = true
+
+def visit_with_onmiauth(path:, user:)
+  OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({ provider: 'github',
+                                                                uid: user.id,
+                                                                info: { nickname: user.login,
+                                                                        name: user.name,
+                                                                        image: user.avatar_url } })
+  visit root_path
+  click_button 'ログイン'
+  visit path
+end
