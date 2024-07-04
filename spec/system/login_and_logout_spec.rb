36a9f3f95bb9bcfaa38408603ec24d21e30a9e31
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-07-04 15:32:30 +0900

    ログイン＆ログアウトのシステムスペックの書き方を微調整

--- spec/system/login_and_logout_spec.rb
+++ spec/system/login_and_logout_spec.rb
@@ -6,14 +6,15 @@ RSpec.describe 'Login And Logout', type: :system do
   before do
     allow(ENV).to receive(:[]).and_call_original
     allow(ENV).to receive(:[]).with('REPOSITORY_ID').and_return('123')
-    FactoryBot.create(:repository, id: 123)
-    FactoryBot.create(:user, id: 123_45, login: 'alice')
 
-    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({ provider: 'github',
-                                                                  uid: 123_45,
-                                                                  info: { nickname: 'alice',
-                                                                          name: 'アリス',
-                                                                          image: 'https://example.com/avatar.jpg' } })
+    FactoryBot.create(:repository, id: 123)
+    FactoryBot.create(:user, login: 'alice') do |alice|
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({ provider: 'github',
+                                                                    uid: alice.id,
+                                                                    info: { nickname: alice.login,
+                                                                            name: alice.name,
+                                                                            image: alice.avatar_url } })
+    end
   end
 
   scenario 'ユーザーとしてログインに成功すること' do
@@ -27,9 +28,12 @@ RSpec.describe 'Login And Logout', type: :system do
   context 'ユーザーとして、アカウント登録ボタンをクリックした場合' do
     scenario 'ログインに成功すること' do
       visit root_path
-      expect { click_button 'GitHubアカントで登録' }.not_to(change { User.count })
-      expect(page).to have_current_path '/alice/issues?association=assigned'
-      expect(page).to have_content 'ログインしました'
+      expect do
+        click_button 'GitHubアカントで登録'
+
+        expect(page).to have_current_path '/alice/issues?association=assigned'
+        expect(page).to have_content 'ログインしました'
+      end.not_to(change { User.count })
     end
   end
 
@@ -37,7 +41,7 @@ RSpec.describe 'Login And Logout', type: :system do
     visit root_path
     click_button 'ログイン'
 
-    using_wait_time(5) do
+    using_wait_time(5) do # フラッシュメッセージが閉じるのを待つ
       click_button 'alice'
       click_link 'ログアウト'
     end
