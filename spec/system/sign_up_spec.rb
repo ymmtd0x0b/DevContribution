60660024d4a4651699bbeff6a18eb79e3109d17c
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-07-08 03:09:58 +0900

    サインアップのシステムスペックを作成

--- spec/system/sign_up_spec.rb
+++ spec/system/sign_up_spec.rb
@@ -4,31 +4,50 @@ require 'rails_helper'
 
 RSpec.describe 'Sign up', type: :system do
   before do
-    allow(ENV).to receive(:[]).and_call_original
-    allow(ENV).to receive(:[]).with('REPOSITORY_ID').and_return('123')
+    @tmpdir_realpath = setup_dummpy_repository_wiki
+    FactoryBot.create(:repository, id: 101, name: 'test/repository', url: "#{@tmpdir_realpath}/test_repository")
 
-    FactoryBot.create(:repository, id: 123)
+    allow(ENV).to receive(:[]).and_call_original
+    allow(ENV).to receive(:[]).with('REPOSITORY_ID').and_return('101')
+
     OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({ provider: 'github',
-                                                                  uid: 123_45,
+                                                                  uid: 501,
                                                                   info: { nickname: 'alice',
                                                                           name: 'アリス',
-                                                                          image: 'https://example.com/avatar.jpg' } })
-    allow(Newspaper).to receive(:publish)
+                                                                          image: '' } })
   end
 
-  scenario 'ユーザー登録できること' do
+  after do
+    FileUtils.remove_entry_secure @tmpdir_realpath if Dir.exist?(@tmpdir_realpath)
+  end
+
+  def setup_dummpy_repository_wiki
+    tmpdir = Dir.mktmpdir
+    tmpdir_realpath = File.realpath tmpdir
+    Dir.chdir(tmpdir_realpath) { Git.init('test_repository.wiki.git') }
+
+    tmpdir_realpath
+  end
+
+  scenario 'ユーザー登録できること', vcr: { cassette_name: 'system/sign_up' } do
     visit root_path
-    expect { click_button 'GitHubアカントで登録' }.to change { User.count }.from(0).to(1)
-    expect(page).to have_current_path '/alice/issues?association=assigned'
-    expect(page).to have_content 'アカウント連携に成功しました'
+    expect do
+      click_button 'GitHubアカントで登録'
+      expect(page).to have_current_path '/alice/issues?association=assigned'
+      expect(page).to have_content 'アカウント連携に成功しました'
+      expect(page).to have_content 'alice'
+    end.to change { User.count }.from(0).to(1)
   end
 
   context 'ゲストとして、ログインボタンをクリックした場合' do
-    scenario 'ユーザー登録できること' do
+    scenario 'ユーザー登録できること', vcr: { cassette_name: 'system/sign_up' } do
       visit root_path
-      expect { click_button 'ログイン' }.to change { User.count }.from(0).to(1)
-      expect(page).to have_current_path '/alice/issues?association=assigned'
-      expect(page).to have_content 'アカウント連携に成功しました'
+      expect do
+        click_button 'ログイン'
+        expect(page).to have_current_path '/alice/issues?association=assigned'
+        expect(page).to have_content 'アカウント連携に成功しました'
+        expect(page).to have_content 'alice'
+      end.to change { User.count }.from(0).to(1)
     end
   end
 end
