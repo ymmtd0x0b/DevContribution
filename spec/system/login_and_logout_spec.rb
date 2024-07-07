6ea5d55a31b8adddcd66e8186605ba75f8401441
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-07-08 03:12:42 +0900

    fixup ログインのシステムスペック

--- spec/system/login_and_logout_spec.rb
+++ spec/system/login_and_logout_spec.rb
@@ -4,49 +4,51 @@ require 'rails_helper'
 
 RSpec.describe 'Login And Logout', type: :system do
   before do
+    FactoryBot.create(:repository, id: 123)
     allow(ENV).to receive(:[]).and_call_original
     allow(ENV).to receive(:[]).with('REPOSITORY_ID').and_return('123')
 
-    FactoryBot.create(:repository, id: 123)
-    FactoryBot.create(:user, login: 'alice') do |alice|
-      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({ provider: 'github',
-                                                                    uid: alice.id,
-                                                                    info: { nickname: alice.login,
-                                                                            name: alice.name,
-                                                                            image: alice.avatar_url } })
-    end
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({ provider: 'github',
+                                                                  uid: 123,
+                                                                  info: { nickname: 'alice',
+                                                                          name: '',
+                                                                          image: '' } })
   end
 
-  scenario 'ユーザーとしてログインに成功すること' do
+  scenario 'ログインに成功すること' do
+    FactoryBot.create(:user, id: 123, login: 'alice')
+
     visit root_path
     click_button 'ログイン'
-
     expect(page).to have_current_path '/alice/issues?association=assigned'
     expect(page).to have_content 'ログインしました'
   end
 
   context 'ユーザーとして、アカウント登録ボタンをクリックした場合' do
     scenario 'ログインに成功すること' do
+      FactoryBot.create(:user, id: 123, login: 'alice')
+
       visit root_path
       expect do
         click_button 'GitHubアカントで登録'
-
         expect(page).to have_current_path '/alice/issues?association=assigned'
         expect(page).to have_content 'ログインしました'
       end.not_to(change { User.count })
     end
   end
 
-  scenario 'ユーザーとしてログアウトに成功すること' do
+  scenario 'ログアウトに成功すること' do
+    FactoryBot.create(:user, id: 123, login: 'alice')
+
     visit root_path
     click_button 'ログイン'
+    expect(page).to have_content 'ログインしました'
 
     using_wait_time(5) do # フラッシュメッセージが閉じるのを待つ
       click_button 'alice'
       click_link 'ログアウト'
     end
-
-    expect(page).to have_current_path '/'
+    expect(page).to have_current_path root_path
     expect(page).to have_content 'ログアウトしました'
   end
 end
