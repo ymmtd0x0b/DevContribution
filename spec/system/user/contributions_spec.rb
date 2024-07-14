a752918b8039d9ff6d9a3712f398f63ae843d471
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-07-15 02:00:26 +0900

    テスト内の無駄なコードを削除

--- spec/system/user/contributions_spec.rb
+++ spec/system/user/contributions_spec.rb
@@ -4,39 +4,26 @@ require 'rails_helper'
 
 RSpec.describe 'User::Contributions', type: :system do
   before do
-    @tmpdir_realpath = setup_dummpy_repository_wiki
-    FactoryBot.create(:repository, id: 123, name: 'test/repository', url: "#{@tmpdir_realpath}/test_repository")
+    FactoryBot.create(:repository, id: 123, name: 'test/repository')
 
     allow(ENV).to receive(:[]).and_call_original
     allow(ENV).to receive(:[]).with('REPOSITORY_ID').and_return('123')
   end
 
-  def setup_dummpy_repository_wiki
-    tmpdir = Dir.mktmpdir
-    tmpdir_realpath = File.realpath tmpdir
-    Dir.chdir(tmpdir_realpath) { Git.init('test_repository.wiki.git') }
+  scenario 'ユーザーの 作成/担当/レビューした Issue とそれと紐付いた PullRequest 、Wiki を一覧表示すること' do
+    alice = FactoryBot.create(:user, id: 456, login: 'alice')
 
-    tmpdir_realpath
-  end
-
-  after do
-    FileUtils.remove_entry_secure @tmpdir_realpath if Dir.exist?(@tmpdir_realpath)
-  end
-
-  scenario 'ユーザーに関連する Issue や PR を FBC の提出物フォーマットに従って、一覧表示を行う' do
-    FactoryBot.create(:user, id: 456, login: 'alice') do |alice|
-      FactoryBot.create(:issue, title: 'アリスが担当した Issue') do |issue|
-        issue.assigns.create!(user: alice)
-        issue.resolutions.create!(pull_request: FactoryBot.create(:pull_request, number: 111) { |pr| pr.assigns.create!(user: alice) })
-      end
-
-      FactoryBot.create(:issue, title: 'アリスがレビューした Issue') do |issue|
-        issue.resolutions.create!(pull_request: FactoryBot.create(:pull_request, number: 222) { |pr| pr.reviews.create!(user: alice) })
-      end
-
-      FactoryBot.create(:issue, title: 'アリスが作成した Issue', user: alice)
-      FactoryBot.create(:wiki, title: 'アリスが作成した Wiki', user: alice)
+    FactoryBot.create(:issue, title: 'アリスが担当した Issue') do |issue|
+      issue.assigns.create!(user: alice)
+      issue.resolutions.create!(pull_request: FactoryBot.create(:pull_request, number: 111) { |pr| pr.assigns.create!(user: alice) })
     end
+
+    FactoryBot.create(:issue, title: 'アリスがレビューした Issue') do |issue|
+      issue.resolutions.create!(pull_request: FactoryBot.create(:pull_request, number: 222) { |pr| pr.reviews.create!(user: alice) })
+    end
+
+    FactoryBot.create(:issue, title: 'アリスが作成した Issue', user: alice)
+    FactoryBot.create(:wiki, title: 'アリスが作成した Wiki', user: alice)
 
     visit_with_onmiauth(path: users_contributions_path('alice'), user: alice)
     expect(page).to have_button('Copy')
