e077ad656c6e25431c5503fe340f97ed67f922c6
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-07-17 11:01:29 +0900

    リファクタリング

--- spec/system/retirements_spec.rb
+++ spec/system/retirements_spec.rb
@@ -4,16 +4,16 @@ require 'rails_helper'
 
 RSpec.describe 'Retirements', type: :system do
   before do
-    FactoryBot.create(:repository, id: 123)
     allow(ENV).to receive(:[]).and_call_original
     allow(ENV).to receive(:[]).with('REPOSITORY_ID').and_return('123')
+
+    FactoryBot.create(:repository, id: 123)
   end
 
   scenario '退会できること' do
     alice = FactoryBot.create(:user, login: 'alice')
 
-    visit_with_omniauth(path: root_path, user: alice)
-
+    login_as alice
     expect do
       click_button 'alice'
       click_link 'アカウントを削除'
