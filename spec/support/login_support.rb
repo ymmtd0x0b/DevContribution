f07ae8f5a52ba336fdfeb7efecce2a58e21071c4
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-07-17 10:09:26 +0900

    システムスペック用のログインサポートを作成

---
+++ spec/support/login_support.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module LoginSupport
+  def login_as(user)
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({ provider: 'github',
+                                                                  uid: user.id,
+                                                                  info: { nickname: user.login,
+                                                                          name: user.name,
+                                                                          image: user.avatar_url } })
+    visit root_path
+    click_button 'ログイン'
+  end
+end
+
+RSpec.configure do |config|
+  config.include LoginSupport
+end
