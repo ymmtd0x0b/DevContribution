f7e48f2b4d1f3c417a0c85b641da940aa616673e
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-07-17 10:57:17 +0900

    リファクタリング

--- spec/system/login_and_logout_spec.rb
+++ spec/system/login_and_logout_spec.rb
@@ -4,51 +4,44 @@ require 'rails_helper'
 
 RSpec.describe 'Login And Logout', type: :system do
   before do
-    FactoryBot.create(:repository, id: 123)
     allow(ENV).to receive(:[]).and_call_original
     allow(ENV).to receive(:[]).with('REPOSITORY_ID').and_return('123')
 
-    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({ provider: 'github',
-                                                                  uid: 456,
-                                                                  info: { nickname: 'alice',
-                                                                          name: '',
-                                                                          image: '' } })
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({ provider: 'github', uid: 456, info: { nickname: 'alice', name: '', image: '' } })
+
+    FactoryBot.create(:repository, id: 123)
+    FactoryBot.create(:user, id: 456, login: 'alice')
   end
 
   scenario 'ログインに成功すること' do
-    FactoryBot.create(:user, id: 456, login: 'alice')
-
     visit root_path
     click_button 'ログイン'
+
     expect(page).to have_current_path '/alice/issues?association=assigned'
     expect(page).to have_content 'ログインしました'
   end
 
   context 'ユーザーとして、アカウント登録ボタンをクリックした場合' do
     scenario 'ログインに成功すること' do
-      FactoryBot.create(:user, id: 456, login: 'alice')
-
       visit root_path
-      expect do
-        click_button 'GitHubアカントで登録'
-        expect(page).to have_current_path '/alice/issues?association=assigned'
-        expect(page).to have_content 'ログインしました'
-      end.not_to(change { User.count })
+      click_button 'GitHubアカントで登録'
+
+      expect(page).to have_current_path '/alice/issues?association=assigned'
+      expect(page).to have_content 'ログインしました'
     end
   end
 
   scenario 'ログアウトに成功すること' do
-    FactoryBot.create(:user, id: 456, login: 'alice')
-
     visit root_path
     click_button 'ログイン'
     expect(page).to have_content 'ログインしました'
 
-    using_wait_time(5) do # フラッシュメッセージが閉じるのを待つ
+    using_wait_time(5) do # フラッシュメッセージとログアウトボタンが重なるので、メッセージが閉じるまで待つ
       click_button 'alice'
       click_link 'ログアウト'
     end
-    expect(page).to have_current_path root_path
+
+    expect(page).to have_current_path '/'
     expect(page).to have_content 'ログアウトしました'
   end
 end
