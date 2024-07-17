f2c71e96a1246ee69022563a0b25c5d812f03079
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-07-17 11:11:16 +0900

    リファクタリング

--- spec/system/user/contributions_spec.rb
+++ spec/system/user/contributions_spec.rb
@@ -4,28 +4,30 @@ require 'rails_helper'
 
 RSpec.describe 'User::Contributions', type: :system do
   before do
-    FactoryBot.create(:repository, id: 123, name: 'test/repository')
-
     allow(ENV).to receive(:[]).and_call_original
     allow(ENV).to receive(:[]).with('REPOSITORY_ID').and_return('123')
+
+    FactoryBot.create(:repository, id: 123, name: 'test/repository')
   end
 
   scenario 'ユーザーの 作成/担当/レビューした Issue とそれと紐付いた PullRequest 、Wiki を一覧表示すること' do
     alice = FactoryBot.create(:user, id: 456, login: 'alice')
 
     FactoryBot.create(:issue, title: 'アリスが担当した Issue') do |issue|
-      issue.assigns.create!(user: alice)
-      issue.resolutions.create!(pull_request: FactoryBot.create(:pull_request, number: 111) { |pr| pr.assigns.create!(user: alice) })
+      issue.assignee << alice
+      issue.pull_requests << FactoryBot.create(:pull_request, number: 111) { |pr| pr.assignees << alice }
     end
 
     FactoryBot.create(:issue, title: 'アリスがレビューした Issue') do |issue|
-      issue.resolutions.create!(pull_request: FactoryBot.create(:pull_request, number: 222) { |pr| pr.reviews.create!(user: alice) })
+      issue.pull_requests << FactoryBot.create(:pull_request, number: 222) { |pr| pr.reviewers << alice }
     end
 
     FactoryBot.create(:issue, title: 'アリスが作成した Issue', user: alice)
     FactoryBot.create(:wiki, title: 'アリスが作成した Wiki', user: alice)
 
-    visit_with_omniauth(path: users_contributions_path('alice'), user: alice)
+    login_as alice
+    visit users_contributions_path alice.login
+
     expect(page).to have_button('Copy')
     expect(page).to have_link('アリスが担当した Issue')
     expect(page).to have_link('#111')
