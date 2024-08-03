dfdf107d4ee9c9eff9975e6a7b5ae59a4108892f
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-08-04 04:52:04 +0900

    OGPの設定を追加

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -7,7 +7,20 @@ module MetaTagsHelper
       charset: 'utf-8',
       description: 'チーム開発プラクティスでのGitHub上の取り組みを個人に焦点を当てて可視化できるサービスです。',
       keywords: 'GitHub, Issue, PullRequest, チーム開発, フィヨルドブートキャンプ、FBC',
-      reverse: true
+      reverse: true,
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: "#{request.original_url}/ogp/ogp.jpg", # 本番環境では URL を直書きする
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        image: "#{request.original_url}/ogp/ogp.jpg" # 本番環境では URL を直書きする
+      }
     }
   end
 end
