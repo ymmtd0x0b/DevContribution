0d9a28c4c5fd8897db32e87c7c522f2aa4de254f
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-08-14 19:57:12 +0900

    User#assigned_pull_requests/#reviews_pull_requests の共通部分を切り出し

---
+++ app/models/concerns/pull_requests_association_extension.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module PullRequestsAssociationExtension
+  def not_referenced_by_other_users
+    sql = <<~"SQL"
+      #{not_assigned_by_other_users} AND #{not_reviewed_by_other_users}
+    SQL
+    where(sql, owner_id: proxy_association.owner.id)
+  end
+
+  private
+
+  def not_assigned_by_other_users
+    <<~SQL
+      NOT EXISTS (
+        SELECT 1
+        FROM assigns
+        WHERE assigns.assignable_id = pull_requests.id AND assigns.user_id != :owner_id
+      )
+    SQL
+  end
+
+  def not_reviewed_by_other_users
+    <<~SQL
+      NOT EXISTS (
+        SELECT 1
+        FROM reviews
+        WHERE reviews.pull_request_id = pull_requests.id AND reviews.user_id != :owner_id
+      )
+    SQL
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,39 +5,9 @@ class User < ApplicationRecord
 
   has_many :assigns, dependent: :destroy
   has_many :assigned_issues, -> { extending(IssuesAssociationExtension) }, through: :assigns, source: :assignable, source_type: 'Issue'
-  has_many :assigned_pull_requests, through: :assigns, source: :assignable, source_type: 'PullRequest' do
-    def not_referenced_by_other_users
-      sql = <<~SQL
-        NOT EXISTS (
-          SELECT 1
-          FROM assigns
-          WHERE assigns.assignable_id = pull_requests.id AND assigns.user_id != :owner_id
-          UNION
-          SELECT 1
-          FROM reviews
-          WHERE reviews.pull_request_id = pull_requests.id AND reviews.user_id != :owner_id
-        )
-      SQL
-      where(sql, owner_id: proxy_association.owner.id)
-    end
-  end
+  has_many :assigned_pull_requests, -> { extending(PullRequestsAssociationExtension) }, through: :assigns, source: :assignable, source_type: 'PullRequest'
   has_many :reviews, dependent: :destroy
-  has_many :reviewed_pull_requests, through: :reviews, source: :pull_request do
-    def not_referenced_by_other_users
-      sql = <<~SQL
-        NOT EXISTS (
-          SELECT 1
-          FROM assigns
-          WHERE assigns.assignable_id = pull_requests.id AND assigns.user_id != :owner_id
-          UNION
-          SELECT 1
-          FROM reviews
-          WHERE reviews.pull_request_id = pull_requests.id AND reviews.user_id != :owner_id
-        )
-      SQL
-      where(sql, owner_id: proxy_association.owner.id)
-    end
-  end
+  has_many :reviewed_pull_requests, -> { extending(PullRequestsAssociationExtension) }, through: :reviews, source: :pull_request
   has_many :reviewed_issues, -> { extending(IssuesAssociationExtension) }, through: :reviewed_pull_requests, source: :issues
   has_many :wikis, dependent: :destroy
 
