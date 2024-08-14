bf095912c2dc8d37e366562da2c0e184327aba34
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-08-14 14:34:23 +0900

    reviewed_issues 内にメソッドを追加

--- app/models/destroyer/reviewed_issue.rb
+++ app/models/destroyer/reviewed_issue.rb
@@ -2,17 +2,8 @@
 
 module Destroyer
   class ReviewedIssue
-    include Searchable
-
     def call(user)
-      issues_id = user.reviewed_issues.ids
-      issues_id_referenced_by_other_users =
-        filter_issues_assigned_by_other_users_from(issues_id, user.id) +
-        filter_issues_referring_pull_requests_assigned_by_other_users_from(issues_id, user.id) +
-        filter_issues_referring_pull_requests_reviewed_by_other_users_from(issues_id, user.id) +
-        filter_issues_created_by_other_users_that_exist_in_database_from(issues_id, user.id)
-
-      user.reviewed_issues.where.not(id: issues_id_referenced_by_other_users).destroy_all
+      user.reviewed_issues.not_referenced_by_other_users.destroy_all
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -50,7 +50,34 @@ class User < ApplicationRecord
   end
   has_many :reviews, dependent: :destroy
   has_many :reviewed_pull_requests, through: :reviews, source: :pull_request
-  has_many :reviewed_issues, through: :reviewed_pull_requests, source: :issues
+  has_many :reviewed_issues, through: :reviewed_pull_requests, source: :issues do
+    def not_referenced_by_other_users
+      sql = <<~SQL
+        NOT EXISTS (
+          SELECT 1
+          FROM assigns
+          WHERE assigns.assignable_id = issues.id AND assigns.user_id != :owner_id
+        ) AND NOT EXISTS (
+          SELECT 1
+          FROM users
+          WHERE issues.user_id = users.id AND issues.user_id != :owner_id
+        ) AND NOT EXISTS (
+          SELECT 1
+          FROM resolutions
+          INNER JOIN pull_requests ON resolutions.pull_request_id = pull_requests.id
+          INNER JOIN assigns ON assignable_type = 'PullRequest' AND assigns.assignable_id = pull_requests.id
+          WHERE resolutions.issue_id = issues.id AND assigns.user_id != :owner_id
+        ) AND NOT EXISTS (
+          SELECT 1
+          FROM resolutions
+          INNER JOIN pull_requests ON resolutions.pull_request_id = pull_requests.id
+          INNER JOIN reviews ON reviews.pull_request_id = pull_requests.id
+          WHERE resolutions.issue_id = issues.id AND reviews.user_id != :owner_id
+        )
+      SQL
+      where(sql, owner_id: proxy_association.owner.id)
+    end
+  end
 
   has_many :wikis, dependent: :destroy
 
