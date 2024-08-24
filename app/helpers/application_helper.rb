b7f2c41699556461a13d9e897a5ebf3d49bb6dc3
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-08-24 15:28:59 +0900

    Flash の種類によってデザインを変更する

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,13 @@ module ApplicationHelper
       link_to tab_name, path, class: 'inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300'
     end
   end
+
+  def tailwind_classes_for(flash_type)
+    {
+      success: 'bg-green-400 text-white',
+      info: 'bg-sky-400 text-white',
+      warning: 'bg-yellow-400 text-white',
+      error: 'bg-red-400 border-l-4 border-red-700 text-black'
+    }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
 end
