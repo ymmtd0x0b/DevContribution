079a8d05e83f00455cd6e78e234373434cc524fe
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-08-25 04:00:43 +0900

    モーダルが閉じないバグを修正

--- app/javascript/controllers/loading_modal_controller.js
+++ app/javascript/controllers/loading_modal_controller.js
@@ -1,9 +1,14 @@
 import { Controller } from '@hotwired/stimulus'
 import loadingModal from '../loading_modal.js'
+import Swal from 'sweetalert2'
 
 // Connects to data-controller="loading-modal"
 export default class extends Controller {
   connect() {
     loadingModal()
+  }
+
+  disconnect() {
+    Swal.close()
   }
 }
